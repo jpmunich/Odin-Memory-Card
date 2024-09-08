69e2ed2a19ca893b7a055a630c1139e7de10b2e1
jpmunich <parkercoyle8@gmail.com>
2024-09-07 21:18:03 -0400

    Generate background imgs + build app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import PokémonContainer from "./components/Pokémon-Container";
+import Header from "./components/Header";
+import "./styles/App.css";
+
 function App() {
-  return <></>;
+  const rand = Math.floor(Math.random() * 4);
+  let img = "";
+  if (rand === 1) {
+    img = "exeggcute.jpg";
+  } else if (rand === 2) {
+    img = "charizard-card.jpg";
+  } else if (rand === 3) {
+    img = "pikachu.jpg";
+  } else {
+    img = "raichu-desktop.jpg";
+  }
+
+  return (
+    <div style={{ backgroundImage: `url(../../${img})` }} id="app-container">
+      <Header />
+      <div id="pokemon-container-container">
+        <PokémonContainer />
+        <PokémonContainer />
+        <PokémonContainer />
+        <PokémonContainer />
+        <PokémonContainer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
